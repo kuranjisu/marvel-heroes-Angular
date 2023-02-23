e3c5a53b0bfcc6cdaf9ddcbfa11bff942ee23884
Aj <arvi.laco@novare.com.hk>
2023-02-23 14:32:11 +0800

    initial commit

---
+++ src/app/mock-data/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "../interfaces/hero"
+
+export const HEROES: Hero[] = [
+
+    { id: 1, name: 'Black Panther', ability: 'Heightened strength, speed, stamina agility and reflexes; master of martial arts, acrobatics & hand-held weaponry.'},
+    { id: 2, name: 'Spiderman', ability: 'Superhuman strength, agility, endurance, ability to stick to and climb walls and other surfaces, uses self-designed web-shooters allowing him to fire and swing from sticky webs, special "Spider-Sense" warns of incoming danger, genius intellect specializing in chemistry and invention.'},
+    { id: 3, name: 'Iron Man', ability: 'Superhuman strength and durability, flight, and an array of weapons.'},
+    { id: 4, name: 'Hulk', ability: 'Incredible superhuman strength, durability, and healing factor.'},
+    { id: 5, name: 'Ant-Man', ability: 'Ant-Man initially had the power to shrink himself (and other people and objects along with himself) to the size of an ant and return to normal. Over time, he has acquired the ability to change size at will.'}
+];
